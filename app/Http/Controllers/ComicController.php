e6887cfdad29d50f32f42d98ee1a6127505f6669
DavideTessari <davtex@gmail.com>
2024-05-23 16:37:19 +0200

    complete Controller

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -1,10 +1,43 @@
-<?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Comic;
 
 class ComicController extends Controller
 {
-    //
+    public function index()
+    {
+        $comics = Comic::all();
+        return view('comics.index', compact('comics'));
+    }
+
+    public function show(Comic $comic)
+    {
+        return view('comics.show', compact('comic'));
+    }
+
+    public function create()
+    {
+        return view('comics.create');
+    }
+
+    public function store(Request $request)
+    {
+        // Validazione dei dati
+        $validatedData = $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'thumb' => 'required|url',
+            'price' => 'required|numeric',
+            'series' => 'required|string|max:255',
+            'sale_date' => 'required|date',
+            'type' => 'required|string|max:255',
+        ]);
+
+        // Creazione del nuovo fumetto
+        Comic::create($validatedData);
+
+        // Redirect all'elenco dei fumetti
+        return redirect()->route('comics.index')->with('success', 'Comic created successfully!');
+    }
 }
