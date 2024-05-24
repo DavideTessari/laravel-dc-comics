ccc80262bb64787386af2570e6e23f3b8a601269
DavideTessari <davtex@gmail.com>
2024-05-24 18:30:29 +0200

    add edite modify routes and view

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::resource('/comics', ComicController::class)->except(['comics.index']);
 Route::get('/comics/{comic}', [ComicController::class, 'show'])->name('comics.show');
 Route::get('/comics/create', [ComicController::class, 'create'])->name('comics.create');
 Route::post('/comics', [ComicController::class, 'store'])->name('comics.store');
+Route::get('/comics/{comic}/edit', [ComicController::class, 'edit'])->name('comics.edit');
 
 
 ?>
